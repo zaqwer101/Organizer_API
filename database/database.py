a256219c04a22b70883ce7a3dfe26930aa29d520
zaqwer101 <platonov256@gmail.com>
2020-04-13 20:09:19 +0800

    #11, #13

--- database/database.py
+++ database/database.py
@@ -14,7 +14,7 @@ def error(message, code):
     return make_response(jsonify({"error": message}), code)
 
 
-def check_params(params_get, params_post):
+def check_params(params_get=None, params_post=None, params_delete=None):
     def __check_params(func):
         @functools.wraps(func)
         def check_params_inner(*args, **kwargs):
@@ -26,6 +26,10 @@ def check_params(params_get, params_post):
                 for param in params_post:
                     if not param in request.get_json().keys():
                         return error("incorrect POST input", 400)
+            if request.method == 'DELETE':
+                for param in params_delete:
+                    if not param in request.get_json().keys():
+                        return error("incorrect DELETE input", 400)
             return func(*args, **kwargs)
 
         return check_params_inner
@@ -35,9 +39,10 @@ def check_params(params_get, params_post):
 # curl "http://127.0.0.1:5002?collection=shopping_list&database=organizer"
 # curl --header "Content-Type: application/json" --request POST --data '{ "collection": "shopping_list", "database": "organizer", "data": [{"name":"test4", "user": "zaqwer101"}]}' http://127.0.0.1:5002 -k
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST', 'DELETE'])
 @check_params(params_get=['database', 'collection'],
-              params_post=['database', 'collection', 'data'])
+              params_post=['database', 'collection', 'data'],
+              params_delete=['database', 'collection', 'data'])
 def database_handler():
     # получаем данные из БД
     # query=get
@@ -74,5 +79,18 @@ def database_handler():
                 app.logger.info(elem)
                 out.append(str(collection.insert_one(elem).inserted_id))
             return make_response(jsonify({"output": out}), 201)  # объект создан
+        else:
+            return error("empty data", 400)
+    elif request.method == 'DELETE':
+        db_name = request.get_json()['database']
+        collection_name = request.get_json()['collection']
+        db = client[db_name]
+        collection = db[collection_name]
+        data = request.get_json()['data']
+        app.logger.info(data)
+        if len(data) != 0:
+            for elem in request.get_json()['data']:
+                count = collection.delete_many(elem).deleted_count
+            return make_response(jsonify({"status": "success", "deleted": count}), 201)
         else:
             return error("empty data", 400)
